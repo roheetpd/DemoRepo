4fac3fb484d9ac08d0683005c1280e8ca61a26dc
Rohit Digraskar <roheetpd@gmail.com>
2022-01-05 18:28:36 +0530

    This is the latest commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Hello
 {
 	public static void main(String args[]){
-		System.out.println("hello java");
+		System.out.println("hello this is a java program and the code has been updated");
 	}
 
 }
